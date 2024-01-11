548ce3fb7f169defb2f1569cf72f95579117afb6
zhazije <473172339@qq.com>
2024-01-12 02:06:00 +0800

    vue

---
+++ TodoMVC/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app">
+        <h1>TodoMVC</h1>
+    </div>
+
+</template>
+
+
+<script lang="ts">
+    export default{
+        name:'APP'
+    }
+</script>
+
+
+<style>
+
+</style>
